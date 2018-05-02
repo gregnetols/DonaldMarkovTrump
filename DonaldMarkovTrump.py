3b6cd53a403ac33a1d687db761880fec19d49696
Greg <gregnetols@gmail.com>
2018-05-02 00:07:00 -0500

    Put the tweets into the markov chain

--- DonaldMarkovTrump.py
+++ DonaldMarkovTrump.py
@@ -1,17 +1,28 @@
 import HistoricTweets
-
+from MarkovNode import MarkovProcess
 
 def main():
 
     historic_tweets = HistoricTweets.tweet_history()
 
     for idx, tweet in enumerate(historic_tweets):
-        print(idx, tweet.id)
+        #print(idx, tweet.id)
+        pass
+    markov_chain = MarkovProcess()
 
-
-
+    for tweet in historic_tweets:
+        split_tweet = tweet.text.split()
+        for idx, word in enumerate(split_tweet):
+            if idx == 0:
+                pass
+            elif idx == len(split_tweet):
+                markov_chain.add_transition(word, '')
+            else:
+                markov_chain.add_transition(split_tweet[idx-1], word)
 
 
 if __name__ == "__main__":
     main()
+
+
 
